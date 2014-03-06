79eeb4761f66438d17ce0c659e39eac4ee277b22
Nicolas Rodas <nicolasrodas@Polaris.local>
2014-03-06 00:57:43 -0300

    Level 4 complete bumping into final captive enemy

--- turn_action.rb
+++ turn_action.rb
@@ -56,17 +56,17 @@ class NeutralizeEnemy < TurnAction
 
   private
 
-    def search_for_enemy
-      Warrior.current.walk!(Warrior.current.direction_of self.class.enemy_list.first)
-    end
+  def search_for_enemy
+    Warrior.current.walk!(Warrior.current.direction_of self.class.enemy_list.first)
+  end
 
-    def bind_enemy
-      Warrior.current.bind!(Warrior.current.enemies_around_me.first)
-    end
+  def bind_enemy
+    Warrior.current.bind!(Warrior.current.enemies_around_me.first)
+  end
 
-    def hit_first_closest_enemy
-      Warrior.current.attack! Warrior.current.enemies_around_me.first
-    end
+  def hit_first_closest_enemy
+    Warrior.current.attack! Warrior.current.enemies_around_me.first
+  end
 end
 
 class HandleCaptive < TurnAction
@@ -75,11 +75,32 @@ class HandleCaptive < TurnAction
     @captive_list = captive_list
   end
 
+  def self.any_captives?
+    not @captive_list.empty?
+  end
+
+  def self.captive_list
+    @captive_list
+  end
+
   def perform
-    if Warrior.current.captives_around_me.empty?
-      Warrior.current.walk_to_stairs
+    if self.class.any_captives?
+      if Warrior.current.captives_around_me.any?
+        rescue_closest_captive
+      else
+        search_for_captive
+      end
     else
-      Warrior.current.rescue!(Warrior.current.captives_around_me.first)
+      Warrior.current.walk_to_stairs
     end
+  end
+
+  private
+  
+  def search_for_captive
+    Warrior.current.walk!(Warrior.current.direction_of self.class.captive_list.first)
+  end
+  def rescue_closest_captive
+    Warrior.current.rescue!(Warrior.current.captives_around_me.first)
   end
 end
